5e203f1f51f02699f36af1d4f1ba71c0fa8afdbb
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2020-07-04 13:19:54 +0800

    Add ifCond helper

---
+++ config/handlebars-helpers.js
@@ -0,0 +1,11 @@
+const moment = require('moment-timezone')
+moment.locale('zh-tw')
+
+module.exports = {
+  ifCond: function (a, b, options) {
+    if (a === b) {
+      return options.fn(this)
+    }
+    return options.inverse(this)
+  }
+}
